963ada0ca66957f908f2882ecb35074efef20e18
Allison Silveira <allisonsilveiradev@gmail.com>
2022-11-07 19:28:02 -0300

    correção

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
-  alert("hello world");
+  $("header button").click(function () {
+    $("form").slideDown();
+  });
+
+  $("#btn-cancelar").click(function () {
+    $("form").slideUp();
+  });
+
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+
+    const enderecoNovaImagem = $("#endereco-imagem-nova").val();
+    const novoItem = $("<li style='display: none'></li>");
+
+    $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+
+    $(` 
+    <div class="overlay-image-link">
+      <a href="${enderecoNovaImagem}" title="Ver imagem" targe="_blank">Ver imagem</a>
+    </div>`).appendTo(novoItem);
+
+    $(novoItem).appendTo("ul");
+    $(novoItem).fadeIn(1000);
+
+    $("#endereco-imagem-nova").val("");
+  });
 });
